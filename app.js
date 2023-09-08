b47ec0ca6df65434c55b228b30988d43ba080f72
Success Abhulimen <abhulimensuccess@gmail.com>
2023-09-08 15:14:32 +0100

    stage one backend task

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-const express = require('express');
+const express = require("express");
 
-const PORT = 3000
+const PORT = 3000;
 const app = express();
 
+app.get("/api",  (req, res) => {
+  const slack_name = req.query.slack_name;
+  const track = req.query.track;
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const current_day = weekday[new Date().getDay()];
+  const current_utc_time = new Date().toISOString();
+  const github_file_url =
+    "https://github.com/otimebhor/HNGX-Internship-Stage-One/blob/main/app.js";
+  const github_repo_url =
+    "https://github.com/otimebhor/HNGX-Internship-Stage-One";
+const status_code = 200
 
-app.get('/api', (req, res) => {
-    const slack_name = req.query.slack_name;
-    const track = req.query.track;
- const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
- const current_day = weekday[new Date().getDay()];
- const current_utc_time = new Date().toISOString();
- 
- 
-    
+    return res.json({
+        "slack_name" : slack_name,
+        "current_day" : current_day,
+        "utc_time" : current_utc_time,
+        "track" : track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": status_code
+    })
+});
 
-})
-
-app.listen(PORT, ()=> {
-    console.log(`Server listening on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
